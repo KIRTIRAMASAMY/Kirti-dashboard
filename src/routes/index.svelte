08cf9403f69738b9b6cf31aaae9c97b6da0ade8d
KIRTIRAMASAMY <bkalaivani80@gmail.com>
2022-03-06 02:23:01 +0000

    part13

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -53,189 +53,49 @@ if (day==="Monday") {
   function deleteTimeSlot(day,index){
 	 timetable[day].splice(index,1);
 	 timetable=timetable;
+	 saveEntry();
   }
 
   function setTimeSlot(day,index,newName,newPeriod,newStyle){
 	  timetable [day] [index] . name = newName ;
 	  timetable [day] [index] . period = newPeriod ;
 	  timetable [day] [index] . style = newStyle ;
+	  saveEntry();
   }
+
+  async function saveEntry() {
+  const { error } = await supabase.from("studentEntries").upsert(
+    {
+      user_id: supabase.auth.user().id,
+      timetable: timetable,
+    },
+    { onConflict: "user_id" }
+  );
+  if (error) alert(error.message);
+}
+
+async function getEntries() {
+  const { data, error } = await supabase.from("studentEntries").select();
+  if (error) alert(error.message);
+
+  if (data != "") {
+    timetable = data[0].timetable;
+  }
+}
+
+getEntries();
+
 
 
 
   
 
   let timetable = {
-	Monday: [
-  	{
-    	name: "PH",
-    	period: 1,
-    	style: "",
-  	},
-  	{
-    	name: "PM",
-    	period: 1,
-    	style: "",
-  	},
-  	{
-    	name: "BI",
-    	period: 2,
-    	style: "",
-  	},
-  	{
-    	name: "R",
-    	period: 1,
-    	style: "table-success",
-  	},
-  	{
-    	name: "BM",
-    	period: 2,
-    	style: "",
-  	},
-  	{
-    	name: "M3",
-    	period: 2,
-    	style: "",
-  	},
-  	{
-    	name: "BC",
-    	period: 2,
-    	style: "",
-  	},
-	],
-	Tuesday: [
-  	{
-    	name: "PJPK",
-    	period: 1,
-    	style: "",
-  	},
-  	{
-    	name: "M3",
-    	period: 2,
-    	style: "",
-  	},
-  	{
-    	name: "BI",
-    	period: 1,
-    	style: "",
-  	},
-  	{
-    	name: "E",
-    	period: 1,
-    	style: "table-success",
-  	},
-  	{
-    	name: "BM",
-    	period: 2,
-    	style: "",
-  	},
-  	{
-    	name: "BC",
-    	period: 3,
-    	style: "",
-  	},
-  	{
-    	name: "PJPK",
-    	period: 1,
-    	style: "",
-  	},
-	],
-	Wednesday: [
-  	{
-    	name: "BC",
-    	period: 3,
-    	style: "",
-  	},
-  	{
-    	name: "PM",
-    	period: 1,
-    	style: "",
-  	},
-  	{
-    	name: "H",
-    	period: 1,
-    	style: "table-success",
-  	},
-  	{
-    	name: "PKS",
-    	period: 3,
-    	style: "",
-  	},
-  	{
-    	name: "BM",
-    	period: 2,
-    	style: "",
-  	},
-	],
-	Thursday: [
-  	{
-    	name: "SA",
-    	period: 1,
-    	style: "",
-  	},
-  	{
-    	name: "PJPK",
-    	period: 1,
-    	style: "",
-  	},
-  	{
-    	name: "BM",
-    	period: 2,
-    	style: "",
-  	},
-  	{
-    	name: "A",
-    	period: 1,
-    	style: "table-success",
-  	},
-  	{
-    	name: "PM",
-    	period: 1,
-    	style: "",
-  	},
-  	{
-    	name: "BC",
-    	period: 2,
-    	style: "",
-  	},
-  	{
-    	name: "M3",
-    	period: 2,
-    	style: "",
-  	},
-	],
-	Friday: [
-  	{
-    	name: "BI",
-    	period: 2,
-    	style: "",
-  	},
-  	{
-    	name: "BM",
-    	period: 2,
-    	style: "",
-  	},
-  	{
-    	name: "T",
-    	period: 1,
-    	style: "table-success",
-  	},
-  	{
-    	name: "BC",
-    	period: 2,
-    	style: "",
-  	},
-  	{
-    	name: "PM",
-    	period: 1,
-    	style: "",
-  	},
-  	{
-    	name: "SA",
-    	period: 2,
-    	style: "",
-  	},
-	],
+	Monday: [],
+	Tuesday: [],
+	Wednesday: [],
+	Thursday: [],
+	Friday: [],
   };
 
   async function logout() {
